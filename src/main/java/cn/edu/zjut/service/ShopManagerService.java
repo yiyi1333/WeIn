e5bd4c19a24c7854df881faea236862a54aab330
Hydrogenzyx <69317011+Hydrogenzyx@users.noreply.github.com>
2021-12-24 12:07:26 +0800

    登录不计入日志

--- src/main/java/cn/edu/zjut/service/ShopManagerService.java
+++ src/main/java/cn/edu/zjut/service/ShopManagerService.java
@@ -17,7 +17,6 @@ public class ShopManagerService {
         this.shopManagerImpl = shopManagerImpl;
     }
 
-    @MyLog
     public ShopManager login(ShopManager shopManager) {
         System.out.println("execute --login()-- method.");
         ShopManager managerList = shopManagerImpl.getShopManagerByAccount(shopManager.getShopManagerAccount());
